e3b734899a3e39b285c2d1e126fbb83e7b32d57e
Maria Nushrat Mahia <marianushratmahia@gmail.com>
2026-08-27 20:52:00 +0600

    Update client.py

--- client.py
+++ client.py
@@ -1,1 +1,14 @@
+import socket
 
+HOST = "127.0.0.1"
+PORT = 2121
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+client_socket.connect((HOST, PORT))
+
+message = client_socket.recv(1024).decode()
+
+print("Server:", message)
+
+client_socket.close()
