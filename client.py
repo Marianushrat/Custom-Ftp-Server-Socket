9dfa6cefc2b55dd024ed54a383a620e7b0408ba1
Maria Nushrat Mahia <marianushratmahia@gmail.com>
2026-08-27 22:30:56 +0600

    Update client.py

--- client.py
+++ client.py
@@ -4,11 +4,21 @@ HOST = "127.0.0.1"
 PORT = 2121
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 client_socket.connect((HOST, PORT))
 
 message = client_socket.recv(1024).decode()
-
 print("Server:", message)
+
+username = input("Enter username: ")
+client_socket.sendall(username.encode())
+
+message = client_socket.recv(1024).decode()
+print("Server:", message)
+
+password = input("Enter password: ")
+client_socket.sendall(password.encode())
+
+response = client_socket.recv(1024).decode()
+print("Server:", response)
 
 client_socket.close()
